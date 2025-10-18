8cba9c22b61e24964d25c7c6885e276db117d7f2
Himanshu-gtm-02 <him02052003@gmail.com>
2025-10-18 21:29:18 +0530

    initial upload

---
+++ datatypeConversion.js
@@ -0,0 +1,71 @@
+const score = "33";
+
+console.log(typeof score);
+
+const score1 = "33abc";
+
+console.log(typeof score1);
+
+let valueInNumber = Number(score);
+let valueInNumber1 = Number(score1);
+
+console.log(typeof valueInNumber)
+console.log(typeof valueInNumber1) 
+
+console.log(valueInNumber1) // uper me iska type number dikha rha h , lekin yha agar hum print karaege to "NaN" print krrha h means "number nahi h"
+
+console.log(valueInNumber) //is case me 33 string me tha pehle 1st line me lekin convert ho gya number me.
+
+// conversion into a string
+
+let number = 33;
+let stringNumber = String(number);
+console.log(typeof stringNumber); // this will convert number into a string
+
+//------------------Operations-------------------------
+
+let value = 3;
+let negativeValue = -value;
+console.log(negativeValue);
+
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+// stack and heap memory concept ------------------------
+
+let myYoutubeName = "himanshugautamdotcom"
+let anothername = myYoutubeName
+console.log(anothername)
+anothername = "chaiaurcode"
+console.log(myYoutubeName)
+console.log(anothername)
+
+//-------------------------------------------------------
+const variable = console.log(1);
+console.log(typeof variable);
+
+//-------------JS Concepts-------------------------------
+//int, float , double etc are not valid in JS
+//there is only one numeric type which is "Number"
+
+let a = 2;
+let b = 3.4;
+let c = -5;
+
+console.table([a,b,c]);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+//you can see in the typeof of all the three variables , we get type as Number..
+
+const bigNum = 34565463567834628;
+console.log(typeof bigNum);
+
+const bigNum1 = 34565463567834628n;
+console.log(typeof bigNum);
+//by applying n in the end , you can see that type changes to bigint
+
+
---
+++ test.js
@@ -0,0 +1,11 @@
+// console.log("Hello world");
+
+const accountId = "123";
+let accountName = "Himanshu";
+accountBranch = "Sahpau";
+
+accountName = "HimG";
+console.log(accountName);
+console.log(accountId);
+
+console.table([accountName,accountId,accountBranch]);
